114b10eda64e00849d74f853fe3f169bfba51d47
brahian.verac <brahian.verac@gmail.com>
2019-08-12 21:28:11 -0500

    Add path of files statics

--- back_end/parts_template.py
+++ back_end/parts_template.py
@@ -1,8 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/menu')
 def hello_world():
     return """<ul>
                     <li>
--- init.py
+++ init.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, send_file
+from back_end import parts_template as p_tem
 #Creates a Flask Aplication, name app
 app = Flask(__name__)
 
 @app.route('/index/')
 def index():
+    tst = p_tem.hello_world()
     return render_template('index.html')
 
 #run the aplication
