92e8035134dace173319873e59450e64ea27829b
brahian.verac <brahian.verac@gmail.com>
2019-08-18 23:49:02 -0500

    render all templates

--- init.py
+++ init.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, send_file
 from back_end import parts_template as p_tem
 #Creates a Flask Aplication, name app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route('/index/')
-def index():
-    tst = p_tem.hello_world()
-    return render_template('index.html')
+@app.route('/<name_template>', methods=['GET'])
+def index(name_template):
+    parts_temp = p_tem.parts_templates()
+    return render_template( name_template, parts_temp =  parts_temp.all_parts())
 
 #run the aplication
 if __name__ == "__main__":
