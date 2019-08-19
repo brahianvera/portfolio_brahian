b08097d69be675aa05fd0c1152df114f4e57668e
brahian.verac <brahian.verac@gmail.com>
2019-08-18 23:48:23 -0500

    change file of parts_template

--- back_end/parts_template.py
+++ back_end/parts_template.py
@@ -1,21 +1,32 @@
-def hello_world():
-    return """<ul>
-                    <li>
-                        <a href="index.html">Home</a>
-                    </li>
-                    <li>
-                        <a href="about.html" class="active">About</a>
-                    </li>
-                    <li>
-                        <a href="skills.html">Skills</a>
-                    </li>
-                    <li>
-                        <a href="services.html">Services</a>
-                    </li>
-                    <li>
-                        <a href="portfolio.html">Portofolio</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">contact</a>
-                    </li>
-                </ul>"""
+class parts_templates:
+
+    def all_parts(self):
+        parts = {}
+        parts["menu"] = self.menu()
+        return parts
+
+    def menu(self):
+        return """
+                <nav id="menu" class="panel">
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="about.html" class="active">About</a>
+                        </li>
+                        <li>
+                            <a href="skills.html">Skills</a>
+                        </li>
+                        <li>
+                            <a href="services.html">Services</a>
+                        </li>
+                        <li>
+                            <a href="portfolio.html">Portofolio</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            """
